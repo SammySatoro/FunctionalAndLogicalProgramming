6957c29524cddd045d0f7772e35c66444e850494
SammySatoro <80969335+SammySatoro@users.noreply.github.com>
2022-03-23 02:45:38 +0300

    Update lw_11.fs

--- lab_work_5/lw_11.fs
+++ lab_work_5/lw_11.fs
@@ -3,8 +3,13 @@ open System
 let processAnswer  answer = 
     match answer with
         |"F#"|"Prolog"-> "Sycophant!"
+        |"C++"|"c++"|"Cxx"|"cxx"|"CXX"-> "Just don't talk to me anymore..."
+        |"C#"-> "Already tried Unity?"
+        |"JavaScript"|"javascript"-> "It ain't Java, you know that?"
+        |"Java"-> "I've played Minecraft too"
+        |"Python" -> "..."
         |other -> "You're goddamn psychopath!"
-   
+    
 [<EntryPoint>]
 let main argv =
     printf "Which programming language do you like the most?: " 
