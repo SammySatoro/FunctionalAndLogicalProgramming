134ace4030011f1c3ac8559e0e47ec77ed6f3992
SammySatoro <80969335+SammySatoro@users.noreply.github.com>
2022-03-27 17:38:04 +0300

    Update lw_18.fs

--- lab_work_5/lw18/lw_18.fs
+++ lab_work_5/lw18/lw_18.fs
@@ -30,9 +30,48 @@ let secondMethod n cond =
   loop n cond n % 10
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
+// Third Method vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+let findGCD x y =
+  let rec loop x y =
+    if x * y = 0 then x + y
+    else
+      let nextX = 
+        if x > y then x % y
+        else x
+      let nextY =
+        if x <= y then y % x
+        else y
+      loop nextX nextY
+  loop x y
+
+let processDigits n func oper= 
+  let rec loop n func accum = 
+    if n <= 0 then accum
+    else 
+      let nextAccum = func accum (n % 10)
+      let nextN = n / 10
+      loop nextN func nextAccum
+  loop n func oper
+
+let thirdMethod n =
+  let rec loop n sum curDiv =
+    if curDiv <= 0 then sum
+    else 
+      let nextSum = 
+        sum + (if (n % curDiv = 0 && findGCD (processDigits n (fun x y -> x + y) 0) curDiv = 1 &&  findGCD (processDigits n (fun x y -> x * y) 1) curDiv <> 1) then curDiv else 0) 
+      let nextDiv = curDiv - 1
+      loop n nextSum nextDiv
+  loop n 0 n
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 [<EntryPoint>]
 let main argv =           
+//First method
     let n = (FirstMethod 15 (fun x y -> x + 1) (fun d -> d % 3 <> 0) 0)
     printfn "Number of divisors not divisible by 3: %A" n // 1, 5 (2)
+ //Second method   
     printfn "Minimal odd of number: %A" (secondMethod 515633454 (fun c -> c % 2 <> 0))// (1)
+//Third method
+    printfn "Sum of divisors that are mutually prime with sum and not mutually prime with product of the number's digits: %A" (thirdMethod 14) // 14   1 2 7 14
+    printfn "Sum of digits: %A" (processDigits 14 (fun x y -> x + y) 0) // 5    1 5
+    printfn "Product of digits: %A" (processDigits 14 (fun x y -> x * y) 1) // 4    1 2 4
     0 
