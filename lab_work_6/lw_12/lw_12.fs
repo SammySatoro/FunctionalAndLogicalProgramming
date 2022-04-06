900693dddc3b25ffe7df97428c23932f999be521
SammySatoro <80969335+SammySatoro@users.noreply.github.com>
2022-04-07 01:42:44 +0300

    Create lw_12.fs

---
+++ lab_work_6/lw_12/lw_12.fs
@@ -0,0 +1,31 @@
+open System
+
+let findMin list =
+    let rec loop list min = 
+        match list with
+        |head::tail ->
+            let nextMin = if min >= head then head else min
+            loop tail nextMin
+        |[] -> min
+    loop list list.Head
+ 
+let isGlobalMin list index = 
+    let rec loop list index min is = 
+        match list with
+        |head::tail ->          
+            if index <> 0 && index >= 0 && index < list.Length then 
+                let nextIndex = index - 1
+                loop tail nextIndex min is
+            else
+                if head = min then is = true
+                else is = false          
+        |[] -> is
+    let min = findMin list
+    loop list index min true
+        
+ 
+[<EntryPoint>]
+let main argv =
+
+    printfn "%A" (isGlobalMin [7;4;9;4;-2;4;6] 4)
+    0
