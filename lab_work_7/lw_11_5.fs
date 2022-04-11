80bb96e95c157e67be8fd59f6a54659d1e529adb
SammySatoro <80969335+SammySatoro@users.noreply.github.com>
2022-04-11 21:06:51 +0300

    Update lw_11_5.fs

---
+++ lab_work_7/lw_11_5.fs
@@ -0,0 +1,13 @@
+module lw_11_5
+open System
+
+let rec createList n = List.init(n) (fun i -> Console.ReadLine()|>Int32.Parse)
+
+let isGlobalMin list index=
+    let minItem = List.min list
+    let itemByIndex = List.item index list
+    if minItem = itemByIndex then true
+    else false
+        
+let ForDemo11 = printfn "%A" (isGlobalMin (createList 10) 5)
+    
