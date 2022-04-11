925c5cfb6331a2515c28ba33820b7b7d730514c5
SammySatoro <80969335+SammySatoro@users.noreply.github.com>
2022-04-11 22:16:23 +0300

    Update lw_11_5.fs

--- lab_work_7/lw_11_5.fs
+++ lab_work_7/lw_11_5.fs
@@ -6,8 +6,7 @@ let rec createList n = List.init(n) (fun i -> Console.ReadLine()|>Int32.Parse)
 let isGlobalMin list index=
     let minItem = List.min list
     let itemByIndex = List.item index list
-    if minItem = itemByIndex then true
-    else false
+    minItem = itemByIndex
         
 let ForDemo11 = printfn "%A" (isGlobalMin (createList 10) 5)
     
