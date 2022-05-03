d890ea701372282f2e90de1cf6f6fe5600f64ad7
SammySatoro <80969335+SammySatoro@users.noreply.github.com>
2022-05-03 23:46:01 +0300

    Update lw_19_14.fs

--- lab_work_7/lw_19/lw_19_14.fs
+++ lab_work_7/lw_19/lw_19_14.fs
@@ -3,11 +3,11 @@ module lw_19_14
 let sortByLettersNumber (str:string) =
     let words = (str.Split " ")
     let wordsToChars = Array.map (fun (w:string) -> w.ToCharArray()) words
-    let sortedByNumberOfChars = Array.sortByDescending (fun (word:array<char>) -> Array.countBy id word) wordsToChars
+    let sortedByNumberOfChars = Array.sortByDescending (fun (word:array<char>) -> word) wordsToChars
     let assembledWords = Array.map (fun (word:array<char>) -> System.String.Join("", word)) sortedByNumberOfChars
     let resultingString = System.String.Join(" ", assembledWords)
     resultingString
-    
+  
 
 let ForDemo19_14 =    
     printfn "%A" (sortByLettersNumber "qwer q tyuiopa sd fgh jklz xc c vbn m")
