506b2f62e6cf17eda820a8a038bd73343edd049b
SammySatoro <80969335+SammySatoro@users.noreply.github.com>
2022-05-03 23:46:27 +0300

    Update lw_19_combined.fs

--- lab_work_7/lw_19/lw_19_combined.fs
+++ lab_work_7/lw_19/lw_19_combined.fs
@@ -23,7 +23,7 @@ let isPalindrome (line:string) =
 let sortByLettersNumber (str:string) =
     let words = (str.Split " ")
     let wordsToChars = Array.map (fun (w:string) -> w.ToCharArray()) words
-    let sortedByNumberOfChars = Array.sortByDescending (fun (word:array<char>) -> Array.countBy id word) wordsToChars
+    let sortedByNumberOfChars = Array.sortByDescending (fun (word:array<char>) -> word) wordsToChars
     let assembledWords = Array.map (fun (word:array<char>) -> System.String.Join("", word)) sortedByNumberOfChars
     let resultingString = System.String.Join(" ", assembledWords)
     resultingString
